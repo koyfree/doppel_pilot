430e70fe68595b78776e37e49e494755de092495
koyfree <koyfree@gmail.com>
2025-07-03 12:04:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ if "step" not in st.session_state:
 
 # 단계 1: ID 입력 및 주제 선택
 if st.session_state["step"] == "start":
-    st.title("🧠 AITwinBot 실험 연구\n")
+    st.title("🧠 AITwinBot 실험 연구")
 
     # 1. 사용자 ID 입력
-    user_name = st.text_input("설문 초반에 입력하신 ID를 동일하게 기입해 주세요. ID는 대소문자를 구별합니다. \n잊어 버리신 경우 관리자에게 문의해 주세요:)")
+    user_name = st.text_input("""설문 초반에 입력하신 ID를 동일하게 기입해 주세요. ID는 대소문자를 구별합니다. 
+    잊어 버리신 경우 관리자에게 문의해 주세요:)""")
 
     # 2. 주제 선택
-    st.markdown("#### 대화 주제를 선택해 주세요. \n")
+    st.markdown("#### 대화 주제를 선택해 주세요.")
     col1, col2 = st.columns(2)
     with col1:
         if st.button("🧘 정신 건강"):
