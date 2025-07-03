333afdb4fcd4714bc7159d5b68b850fa576bf848
koyfree <koyfree@gmail.com>
2025-07-03 12:08:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ if "step" not in st.session_state:
 # 단계 1: ID 입력 및 주제 선택
 if st.session_state["step"] == "start":
     st.title("🧠 AITwinBot 실험 연구")
-    st.markdown("<br><br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
     # 1. 사용자 ID 입력
     user_name = st.text_input("설문 초반에 입력하신 ID를 동일하게 기입해 주세요. 잊어 버리신 경우 관리자에게 문의해 주세요:)")
 
     # 2. 주제 선택
-    st.markdown("<br><br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
     st.markdown("#### 대화 주제를 선택해 주세요.")
     col1, col2 = st.columns(2)
     with col1:
